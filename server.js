885299cba2e3dde82cbf182e2797ac62d0194e63
Pranshu <prkumar@ea.com>
2019-01-29 12:25:35 -0800

    add sample code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const {GraphQLSchema} = require('graphql');
+
+const {queryType} = require('./query.js');
+//setting up the port number and express app
+const port = 5000;
+const app = express();
+
+ // Define the Schema
+const schema = new GraphQLSchema({ query: queryType });
+
+//Setup the nodejs GraphQL server
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
+app.listen(port);
+console.log(`GraphQL Server Running at localhost:${port}`);
